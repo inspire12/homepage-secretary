b623e4dc0d79e8356b7e834205865a571bb64432
inspire12 <seo.yeonghak.1992@gmail.com>
2020-08-29 22:51:18 +0900

    기능: 프로젝트 init
    1. jenkins 연동까지 고려
    2. frontend 기본 처리
    3. spring web

---
+++ src/test/java/com/inspire12/secretary/SecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspire12.secretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
