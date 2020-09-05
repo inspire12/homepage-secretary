ab8e1946e30bf5d5c97cf436f4306c3a77fa3a96
inspire12 <seo.yeonghak.1992@gmail.com>
2020-09-05 19:01:23 +0900

    Add Video Streaming API
    1. Add Mono Reactor

---
+++ README.md
@@ -0,0 +1,13 @@
+# ABOUT 
+이 레포지토리는 자바 Spring 공부하면서 기록을 남기는 부분이 포함되어 있습니다. 
+기본적으로 방향이 안잡혀있습니다. 
+감사합니다. 
+
+
+## Video Stream Service 
+
+
+# Tutorial 
+* [Oracle java](https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html)
+
+
---
+++ src/main/java/com/inspire12/secretary/controller/VideoController.java
@@ -0,0 +1,34 @@
+package com.inspire12.secretary.controller;
+
+import com.inspire12.secretary.service.VideoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+//https://github.com/saravanastar/video-streaming
+@RestController
+@RequestMapping("/video")
+public class VideoController {
+    private final VideoService videoStreamService;
+
+    public VideoController(VideoService videoStreamService) {
+        this.videoStreamService = videoStreamService;
+    }
+
+    @GetMapping("/stream/{fileType}/{fileName}")
+    public Mono<ResponseEntity<byte[]>> getStreamVideo(@RequestHeader(value = "Range", required = false) String httpRangeList,
+                                                       @PathVariable("fileType") String fileType,
+                                                       @PathVariable("fileName") String fileName) {
+        return Mono.just(videoStreamService.prepareContent(fileName, fileType, httpRangeList));
+    }
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "test";
+    }
+}
+
